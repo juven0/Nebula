8adae644306eba2d45b9a90cf52470a3fccb3250
eddy juveno <juveno@ananas.airbox.home>
2024-06-30 19:55:37 +0300

    ajout de la methode pour stoper le reseaux

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module nebula
 go 1.22.4
 
 require (
+	github.com/libp2p/go-libp2p v0.35.1
+	github.com/multiformats/go-multiaddr v0.13.0
+)
+
+require (
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -32,9 +37,7 @@ require (
 	github.com/koron/go-ssdp v0.0.4 // indirect
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
 	github.com/libp2p/go-flow-metrics v0.1.0 // indirect
-	github.com/libp2p/go-libp2p v0.35.1 // indirect
 	github.com/libp2p/go-libp2p-asn-util v0.4.1 // indirect
-	github.com/libp2p/go-libp2p-core v0.20.1 // indirect
 	github.com/libp2p/go-msgio v0.3.0 // indirect
 	github.com/libp2p/go-nat v0.2.0 // indirect
 	github.com/libp2p/go-netroute v0.2.1 // indirect
@@ -49,7 +52,6 @@ require (
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-base32 v0.1.0 // indirect
 	github.com/multiformats/go-base36 v0.2.0 // indirect
-	github.com/multiformats/go-multiaddr v0.12.4 // indirect
 	github.com/multiformats/go-multiaddr-dns v0.3.1 // indirect
 	github.com/multiformats/go-multiaddr-fmt v0.1.0 // indirect
 	github.com/multiformats/go-multibase v0.2.0 // indirect
--- main.go
+++ main.go
@@ -1,86 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/network"
-	"github.com/libp2p/go-libp2p/core/peer"
-	peerstore "github.com/libp2p/go-libp2p/core/peerstore"
-	"github.com/multiformats/go-multiaddr"
+	"nebula/pkg/p2p"
 )
 
 func main() {
-	ctx := context.Background()
-	//creat new hote
-	h, err := libp2p.New(
-		libp2p.ListenAddrStrings(
-			"/ip4/0.0.0.0/tcp/0",
-			"/ip6/::/tcp/0",
-		),
-		libp2p.NATPortMap(),
-		libp2p.EnableRelay(),
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	h.SetStreamHandler("/p2p/1.0.0", func(s network.Stream) {
-		fmt.Println("new flux of :", s.Conn().RemotePeer())
-		defer s.Close()
-	})
-
-	// set personnalised protocole
-	h.SetStreamHandler("/p2p/1.0.0", handelStream)
-	for _, addr := range h.Addrs() {
-		fmt.Printf("Adresse d'écoute: %s\n", addr)
-	}
-	fmt.Printf("ID de pair: %s\n", h.ID())
-
-	if len(os.Args) > 1 {
-		targetAddr, _ := multiaddr.NewMultiaddr(os.Args[1])
-		peerinfo, _ := peer.AddrInfoFromP2pAddr(targetAddr)
-		h.Peerstore().AddAddrs(peerinfo.ID, peerinfo.Addrs, peerstore.PermanentAddrTTL)
-		if err := h.Connect(ctx, *peerinfo); err != nil {
-			log.Println("Erreur lors de la connexion:", err)
-			return
-		}
-		sendMessage(h, *peerinfo, "Bonjour depuis "+h.ID().String())
-	}
-
-	select {}
-
-}
-
-func handelStream(s network.Stream) {
-	fmt.Println("new flux of :", s.Conn().RemotePeer())
-	buf := make([]byte, 1024)
-	n, err := s.Read(buf)
-	if err != nil {
-		log.Println("Erreur lors de la lecture du flux:", err)
-		return
-	}
-	fmt.Printf("Reçu: %s\n", string(buf[:n]))
-	s.Close()
-}
-
-func sendMessage(h host.Host, target peer.AddrInfo, message string) {
-	s, err := h.NewStream(context.Background(), target.ID, "/p2p/1.0.0")
-	if err != nil {
-		log.Println("Erreur lors de la création du flux:", err)
-		return
-	}
-
-	_, err = s.Write([]byte(message))
-	if err != nil {
-		log.Println("Erreur lors de l'envoi du message:", err)
-		s.Close()
-		return
-	}
-	s.Close()
+	network := p2p.P2pNetwork{}
+	network.Network()
 }
