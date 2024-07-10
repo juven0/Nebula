3af01142fd915a0486a251103bd3e2373d8a2e3a
Eddy Juveno BOCQUELIN <bocquelin@gmail.com>
2024-07-10 10:39:15 +0300

    mise en place du strucutre du DHT

--- pkg/DHT/DHT.go
+++ pkg/DHT/DHT.go
@@ -1,1 +1,34 @@
 package dht
+
+import (
+	"crypto/sha256"
+)
+
+const (
+	IdLength   = 256 / 8
+	BucketSize = 20
+)
+
+type NodeID [IdLength]byte
+
+type Node struct {
+	NodeID NodeID
+	Addr   string
+}
+
+type Bucket struct {
+	Nodes []Node
+}
+
+type RoutingTable struct {
+	Buckets [IdLength * 8]Bucket
+	Self    Node
+}
+
+func GenerateNodeId(data string) NodeID {
+	hash := sha256.Sum256([]byte(data))
+	var ID NodeID
+	copy(ID[:], hash[:])
+
+	return ID
+}
