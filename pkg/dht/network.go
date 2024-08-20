24678774849187367ba83dcb149cac0380972f8e
juven0 <bocquelin@gmail.com>
2024-08-20 15:34:24 +0300

    mise en place des fonctio  network

---
+++ pkg/dht/network.go
@@ -0,0 +1,24 @@
+package maindht
+
+import "github.com/libp2p/go-libp2p/core/peer"
+
+func (dht *DHT) JoinNetwork() error {
+
+}
+
+func (dht *DHT) loadKnowPeers() ([]peer.AddrInfo){
+
+}
+
+func (dht *DHT) discoverLocalPeers()([]peer.AddrInfo, error){
+
+}
+
+func (dht *DHT) queryDNSSeeds() ([]peer.AddrInfo, error) {
+    
+}
+
+func (dht *DHT) saveKnownPeers(peers []peer.AddrInfo) error {
+    
+}
+
