89a285c91ccf43e367516d3fe73e618a3690e3f4
juven0 <bocquelin@gmail.com>
2024-08-20 16:46:08 +0300

    mise en place des fonctio  network

--- pkg/dht/network.go
+++ pkg/dht/network.go
@@ -1,9 +1,29 @@
 package maindht
 
-import "github.com/libp2p/go-libp2p/core/peer"
+import (
+	"fmt"
+
+	"github.com/libp2p/go-libp2p/core/peer"
+)
 
 func (dht *DHT) JoinNetwork() error {
 
+	knowPeers := dht.loadKnowPeers()
+	if len(knowPeers)>0 {
+		return dht.Bootstrap(knowPeers)
+	}
+
+	localPeers, err := dht.discoverLocalPeers()
+	if err == nil && len(localPeers) > 0 {
+        return dht.Bootstrap(localPeers)
+    }
+
+	dnsPeers, err := dht.queryDNSSeeds()
+    if err == nil && len(dnsPeers) > 0 {
+        return dht.Bootstrap(dnsPeers)
+    }
+
+	return fmt.Errorf("failed to join network: no peers found")
 }
 
 func (dht *DHT) loadKnowPeers() ([]peer.AddrInfo){
